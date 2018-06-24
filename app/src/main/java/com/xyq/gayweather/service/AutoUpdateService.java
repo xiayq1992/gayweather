270c42841930c9c1d9f033a649e75599fc056242
xyq <xiayunquan123456@163.com>
2018-06-24 23:18:28 +0800

    配置签名

--- app/src/main/java/com/xyq/gayweather/service/AutoUpdateService.java
+++ app/src/main/java/com/xyq/gayweather/service/AutoUpdateService.java
@@ -1,4 +1,91 @@
 package com.xyq.gayweather.service;
 
-public class AutoUpdateService {
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.IBinder;
+import android.os.SystemClock;
+import android.preference.PreferenceManager;
+import android.support.annotation.Nullable;
+
+import com.xyq.gayweather.Constants;
+import com.xyq.gayweather.gson.Weather;
+import com.xyq.gayweather.util.HttpUtil;
+import com.xyq.gayweather.util.Utility;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
+
+public class AutoUpdateService extends Service {
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        updateWeather();
+        updateBingPic();
+
+        AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+        int anHour = 8 * 60 * 60 * 1000;
+        long triggerAtTime = SystemClock.elapsedRealtime() + anHour;
+        Intent i = new Intent(this, AutoUpdateService.class);
+        PendingIntent pi = PendingIntent.getService(this, 0, i, 0);
+        alarmManager.cancel(pi);
+        alarmManager.set(AlarmManager.ELAPSED_REALTIME_WAKEUP, triggerAtTime, pi);
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    private void updateWeather() {
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        String weatherString = preferences.getString("weather", null);
+        if (weatherString != null) {
+            Weather weather = Utility.handleWeatherResponse(weatherString);
+            String weatherId = weather.basic.weatherId;
+            String weatherUrl = Constants.BASE_URL + "weather?cityid=" + weatherId + "&key=" + Constants.WEATHER_KEY;
+            HttpUtil.sendOkhttpRequest(weatherUrl, new Callback() {
+                @Override
+                public void onFailure(Call call, IOException e) {
+                    e.printStackTrace();
+                }
+
+                @Override
+                public void onResponse(Call call, Response response) throws IOException {
+                    String weatherText = response.body().string();
+                    Weather weather1 = Utility.handleWeatherResponse(weatherText);
+                    if (weather1 != null && "ok".equals(weather1.status)) {
+                        SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(AutoUpdateService.this).edit();
+                        editor.putString("weather", weatherText);
+                        editor.apply();
+                    }
+                }
+            });
+        }
+    }
+
+    private void updateBingPic() {
+        String requestUrl = Constants.BASE_URL + "bing_pic";
+        HttpUtil.sendOkhttpRequest(requestUrl, new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                String bingPIc = response.body().string();
+                SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(AutoUpdateService.this).edit();
+                editor.putString("image", bingPIc);
+                editor.apply();
+            }
+        });
+    }
 }
